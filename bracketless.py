07638699102894b52a86e762d9c1f774679022cd
DukeOfFlames <jetdrp@gmail.com>
2022-08-15 01:10:23 +0200

    Added CommittedDeadlySinError to Error-list

--- bracketless.py
+++ bracketless.py
@@ -1086,7 +1086,7 @@ class File:
         self.position += 6
 
     def parse_error_keyword(self):
-        self.errors = ["Exception"]
+        self.errors = ["Exception", "CommittedDeadlySinError"]
         lens = []
 
         for error in self.errors:
