4494f298c514ec92d58a05600e0d53cafa6e9467
DukeOfFlames <jetdrp@gmail.com>
2022-08-23 21:48:03 +0200

    Modify run.py for .runconfig-support

--- run.py
+++ run.py
@@ -1,51 +1,34 @@
 import subprocess
 
+
 class TestType:
     Broken = 0
     Silent = 1
     Visible = 2
 
-visible_tests = [
-]
 
-silent_tests = [
-    "if_test",
-    "manual_factorial",
-    "complex_list_literal",
-    "runnable_list_literal",
-    "double_negation",
-    "list_indexing",
-    "assignment",
-    "scope_test",
-    "comments",
-    "functions",
-    "declarations",
-    "dot",
-    "factorial",
-    "drucke",
-    "abc",
-    "builtin_functions",
-    "negative_numbers",
-    "multiple_parameters",
-]
+with open('visible.runconfig', 'rt') as f_in:
+    visible_tests = f_in.read().split('\n')
 
-broken_tests = [
-    "type_assignment",
-    "brack",
-    "for_test",
-    "try",
-    "library_imports",
-]
+with open('silent.runconfig', 'rt') as f_in:
+    silent_tests = f_in.read().split('\n')
+
+with open('broken.runconfig', 'rt') as f_in:
+    broken_tests = f_in.read().split('\n')
 
 tests = []
-for (test_type, lst) in [(TestType.Visible, visible_tests), (TestType.Silent, silent_tests), (TestType.Broken, broken_tests)]:
+for (test_type, lst) in [(TestType.Visible, visible_tests), (TestType.Silent, silent_tests),
+                         (TestType.Broken, broken_tests)]:
     tests += [(test_type, test_name) for test_name in lst]
 
 for (test_type, test_name) in tests:
+    if test_name == '':
+        continue
     if test_type == TestType.Broken:
         continue
     print(f"Running test: {test_name}")
-    proc = subprocess.run(["python", "bracketless.py", f"Tests/{test_name}.br"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+    proc = subprocess.run(["python", "bracketless.py", f"Tests/{test_name}.br"], stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE, encoding="utf-8")
     with open(f"Tests/{test_name}.out") as f:
         expected_output = f.read()
     did_succeed = proc.returncode == 0 and proc.stdout == expected_output
