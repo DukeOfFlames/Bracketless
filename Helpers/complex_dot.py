9b4fbcf96d33d75606836964ccf6fe5f7402675e
creator1creeper1 <creator1creeper1@airmail.cc>
2022-08-24 22:12:46 +0200

    Add a test with a more complex use of the (.) operator

---
+++ Helpers/complex_dot.py
@@ -0,0 +1,23 @@
+def dot(f):
+    def dot_inner(g):
+        def dot_inner_inner(x):
+            return f(g(x))
+
+        return dot_inner_inner
+
+    return dot_inner
+
+
+def square(x):
+    return x**2
+
+
+def add(x):
+    def add_inner(y):
+        return x + y
+
+    return add_inner
+
+
+res = dot(dot)(dot)(square)(add)(3)(4)
+print(res)
